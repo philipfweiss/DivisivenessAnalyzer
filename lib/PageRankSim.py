c0eff9fd2b879cfbb4b63c9b46f066cbdc68ea53
Philip Weiss <weissp68@stanford.edu>
2018-12-08 23:15:27 -0800

    [update] pageranksim

--- lib/PageRankSim.py
+++ lib/PageRankSim.py
@@ -2,48 +2,165 @@ from DatasetGenerator import *
 import scipy.sparse
 import numpy as np
 import matplotlib.pyplot as plt
+from networkx import *
+from networkx.drawing.nx_agraph import graphviz_layout
 
+
+CURRENT_TOPIC = "Abortion"
+plt.style.use('ggplot')
 
 class PageRankSim:
-    def __init__(self, graph):
+    def __init__(self, graph, teleport, dg):
         self.graph = graph
-        self.teleportmatrix = self.generateTeleportMatrix(beta=0.001)
+        self.dg = dg
+        self.teleport = teleport
+        self.teleportmatrix = self.generateTeleportMatrix(0.1, teleport)
+        self.r = self.powerIteration(4, 100)
 
-    def generateTeleportMatrix(self, beta):
+    def generateTeleportMatrix(self, beta, teleport):
         senators = self.getSenators()
         if len(senators) == 0: return None
-        senators = list(sorted(senators))
+        if teleport is None: teleport = senators
+
+        senators = sorted(list(senators))
         adj_matrix = np.zeros([len(senators), len(senators)])
         numsen = len(senators)
 
-        beta_term =  (1 - beta) / float(len(senators))
         _, graph = self.graph
         for i, si in enumerate(senators):
             deg = sum([graph[(si, sj)] + graph[(sj, si)] for sj in senators])
             for j, sj in enumerate(senators):
                 if (si, sj) not in graph: continue
-                item = max(graph[(si, sj)], graph[(sj, si)]) / float(deg)
-                adj_matrix[i, j] = beta * item + beta_term
+                prob = max(graph[(si, sj)], graph[(sj, si)]) / float(deg)
+                adj_matrix[i, j] = (1 - beta) * prob + (beta if j == teleport else 0)
+
+        ## Now we normalize the columns
+        for i in range(numsen):
+            colsum = sum([adj_matrix[i, j] for j in range(numsen)])
+
         return adj_matrix
 
     def powerIteration(self, senatorIdx, numIters):
-        senators = self.getSenators()
+        senators = sorted(list(self.getSenators()))
         if len(senators) == 0: return None
-        r = np.matrix([1 if i == senatorIdx else 0 for i in range(len(senators))]).T
+        r = np.matrix([1 if i == senatorIdx else 0 for i in range(len(senators))])
         for i in range(numIters):
-            r = np.matmul(self.teleportmatrix, r)
-
-        print(sum(r))
+            r = np.matmul(r, self.teleportmatrix)
+        return r
 
     def getSenators(self):
-        senators = set()
-        _, graph = self.graph
-        for k in graph: senators |= set(k)
-        return senators
+        newsen = set()
+        _, gp = self.graph
+        for k in gp: newsen |= set(k)
+        return newsen
+
+    ## Percent of PPR mass that is same party
+    def findSameParty(self):
+        senators = sorted(self.getSenators())
+        if len(senators) == 0: return None
+        congress, _ = self.graph
+
+        if self.teleport > len(senators): return None
+        # print("FOO", self.teleport, len(senators))
+
+        if (congress, senators[self.teleport]) not in self.dg.member_party_map: return None
+        teleportParty = self.dg.member_party_map[(congress, senators[self.teleport])]
+        congress, _ = self.graph
+        same = 0
+        for idx, prob in enumerate(self.r.T):
+            party = self.dg.member_party_map[(congress, senators[idx])]
+            if party == teleportParty:
+                same += prob
+
+        return same
 
 
-dg = DatasetGenerator(["Israel"], range(96, 114))
+    def visualize(self):
+        # nx.draw(self.teleportmatrix)
+        senators = sorted(self.getSenators())
+        lensen = len(senators)
+        congress, _ = self.graph
+
+        A=numpy.matrix(numpy.zeros((lensen, lensen)))
+        color_map = []
+        for idx, node in enumerate(self.r.T):
+            val = .2 + 190 * node
+            val = val if val < 1 else 1
+            val = val if val > .2 else .2
+            party = self.dg.member_party_map[(congress, senators[idx])]
+            color = 'red' if party == 'Republican' else 'blue'
+            color_map.append(lighten_color(color, float(val)))
+
+        G=nx.from_numpy_matrix(A)
+        nx.draw(G, pos=graphviz_layout(G), node_size=30, node_color = color_map, prog='dot')
+        plt.show()
+
+
+def lighten_color(color, amount=0.5):
+    import matplotlib.colors as mc
+    import colorsys
+    try:
+        c = mc.cnames[color]
+    except:
+        c = color
+    c = colorsys.rgb_to_hls(*mc.to_rgb(c))
+    return colorsys.hls_to_rgb(c[0], 1 - amount * (1 - c[1]), c[2])
+
+
+
+dg = DatasetGenerator([CURRENT_TOPIC], range(70, 115))
 graphs = dg.generate_datasets()
-for graph in graphs["Israel"]:
-    pr = PageRankSim(graph)
-    pr.powerIteration(4, 5)
+
+demsen = collections.defaultdict(int)
+repsen = collections.defaultdict(int)
+for graph in graphs[CURRENT_TOPIC]:
+    strs = set()
+    cong, g = graph
+    for k in g: strs |= set(k)
+    for s in strs:
+        party = dg.member_party_map[(cong, s)]
+        if party == "Republican":
+            repsen[s] += 1
+        else:
+            demsen[s] += 1
+
+
+most_dem = max([(v, k) for k, v in demsen.items()])[1]
+most_rep = max([(v, k) for k, v in repsen.items()])[1]
+
+
+dem_polarization = []
+
+x1, x2 = [], []
+for graph in graphs[CURRENT_TOPIC]:
+    #
+    congress, g = graph
+    for k in g: strs |= set(k)
+    dem_idx = sorted(list(strs)).index(most_dem)
+    print(most_dem)
+    rep_idx = sorted(list(strs)).index(most_rep)
+
+    # print("ROO", rep_idx, most_rep, strs)
+    dpr = PageRankSim(graph, dem_idx, dg)
+    dsim = dpr.findSameParty()
+
+    rpr = PageRankSim(graph, rep_idx, dg)
+    rsim = rpr.findSameParty()
+
+    if rsim is not None:
+        rsim = float(rsim)
+        x1.append((congress * 2 + 1788, rsim))
+    if dsim is not None:
+        dsim = float(dsim)
+        x2.append((congress * 2 + 1788, dsim))
+
+axes = plt.gca()
+fig, ax = plt.subplots()
+ax.axhline(y=0.5, xmin=0.0, xmax=1.0, color='r')
+axes.set_ylim([.15,.85])
+
+plt.title("Personalized Pagerank Polarization for Charles B. Rangel (D. NY) [Abortion]")
+plt.plot([x for (x, _) in x1], [y for (_, y) in x1])
+# plt.plot([x for (x, _) in x2], [y for (_, y) in x2])
+
+plt.show()
--- lib/SpectralPartition.py
+++ lib/SpectralPartition.py
@@ -8,29 +8,53 @@ import scipy.sparse
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.style.use('ggplot')
+
 np.set_printoptions(suppress=True)
 class SpectralPartition:
     def __init__(self, projection):
         print("hello")
-        dg = DatasetGenerator(["Abortion"], range(90, 114))
+        dg = DatasetGenerator(["Israel"], range(96, 114))
         graphs = dg.generate_datasets()
-        # print(graphs)
-        # graphs: map<str issue, list<(congress, map<pair<senator>, int>)>>
-        for graph in graphs["Abortion"]:
-            congress, graph = graph
-            if sum(graph.values()) == 0: continue
-            adj_matrix, senators = self.get_adjacency_matrix(graph)
-            # adj_matrix = self.clean_adjacency_matrix(adj_matrix)
-            laplacian = scipy.sparse.csgraph.laplacian(adj_matrix, normed=True)
-            vals, vecs = np.linalg.eigh(laplacian)
-            colors = []
-            for i, senator in enumerate(senators):
-                # print(senator)
-                colors.append("br"[dg.member_party_map[(congress, senator)] == "Republican"])
-            x, y = vecs[:, 1:3].T
-            plt.scatter(x, y, c=colors)
+        ctr = 0
+        eigen = []
+        ys = []
+        print('loo')
+        for graph in graphs["Israel"]:
+            if sum(graph[1].values()) == 0: continue
+            vals, _  = self.plot_graph(graph, dg, False, ctr, False)
+            eigen.append(vals[1])
+            ys.append(96 + ctr)
+            ctr+=1
+        plt.xlabel('Congress')
+        plt.title('Connectivity of Congressional Voting Graph (Israel)')
+        plt.ylabel('Divisiveness Coefficient (Fiedler Eigenvalue)')
+        plt.plot(ys, eigen)
+        plt.show()
+
+
+    def plot_graph(self, graph, dg, show, ctr, should_plot):
+        congress, graph = graph
+        adj_matrix, senators = self.get_adjacency_matrix(graph)            # adj_matrix = self.clean_adjacency_matrix(adj_matrix)
+        laplacian = scipy.sparse.csgraph.laplacian(adj_matrix, normed=True)
+        vals, vecs = np.linalg.eigh(laplacian)
+        colors = []
+        for i, senator in enumerate(senators):
+            # print(senator)
+            colors.append('dodgerblue' if dg.member_party_map[(congress, senator)] == "Republican" else 'r')
+        x, y = vecs[:, 1:3].T
+
+        if should_plot:
+            ax = plt.subplot(4, 4, 1 + ctr)
+            ctr+=1
+            ax.set_title("Congress %s" % str(96 + ctr) )
+            ax.scatter(x, y, c=colors)
+        else:
+            return (vals, vecs)
+
+        if show:
             plt.show()
-            
+
     def clean_adjacency_matrix(self, adj_matrix):
         adj_matrix = (adj_matrix > np.min(np.max(adj_matrix, axis=0), axis=0)/4).astype(float)
         return adj_matrix
@@ -38,25 +62,19 @@ class SpectralPartition:
     def get_adjacency_matrix(self, graph):
         senators = set()
         for k in graph: senators |= set(k)
-        print(len(senators))
         senators = list(sorted(senators))
         senator_to_index = {k:i for (i, k) in enumerate(senators)}
 
-        print(senators, senator_to_index)
         adj_matrix = np.zeros([len(senators), len(senators)])
-        print(adj_matrix)
         for i, si in enumerate(senators):
             for j, sj in enumerate(senators[i+1:]):
                 item = graph[(si, sj)]
                 # print(item)
                 adj_matrix[i, i+j+1] = item
-                adj_matrix[i+j+1, i] = item 
-        print(adj_matrix)
+                adj_matrix[i+j+1, i] = item
         degrees = np.sum(adj_matrix, axis=0)
-        print(list(sorted(degrees)))
-        adj_matrix = adj_matrix[degrees > 5000,:]
-        adj_matrix = adj_matrix[:, degrees > 5000]
-        print(adj_matrix)
+        # adj_matrix = adj_matrix[degrees > 5000,:]
+        # adj_matrix = adj_matrix[:, degrees > 5000]
         return adj_matrix, senators
 
 SpectralPartition(None)
