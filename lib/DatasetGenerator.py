3bc61f2507ad0eb86fed9293415b1a7b44302679
brianhzhang <zhangbrianh@gmail.com>
2018-12-08 23:01:17 -0500

    nicer visuals

--- lib/DatasetGenerator.py
+++ lib/DatasetGenerator.py
@@ -1,6 +1,8 @@
 import numpy
 import pickle
 import collections
+np = numpy
+
 
 class DatasetGenerator:
 
@@ -8,25 +10,35 @@ class DatasetGenerator:
         self.issues = issues
         self.issue_call_map = self.load_pickle('./pickle/issue_call_map.pkl')
         #print(self.issue_call_map.keys())
+        for k in sorted((len(self.issue_call_map[k]), k) for k in self.issue_call_map):
+            print(k)
 
         ## issueMap[issue][congress] => rollcalls.
         ## call_uid_map[congressId][(id, rollcall#)] => rollcall
         self.issue_map, self.call_uid_map = {}, {}
 
-        self.load_congress_to_call_uid_map()
-        self.load_issue_map()
-        self.call_uid_map = None
+        
 
         # print(len(self.issue_map["Abortion"][112]))
         self.congresses = congresses
+        self.load_congress_to_call_uid_map()
+        self.load_issue_map()
         self.member_party_map = self.load_pickle('./pickle/member_party_map.pkl')
 
     def generate_datasets(self):
         graphs = collections.defaultdict(list)
         for issue in self.issues:
-            for cid in self.congresses:
-                votes = self.generate_dataset(issue, cid)
-                graphs[issue].append((cid, votes))
+            if issue:
+                for cid in self.congresses:
+                    print("loading", issue, cid, len(self.issue_map[issue][cid]))
+                    votes = self.generate_dataset(self.issue_map[issue][cid])
+                    graphs[issue].append((cid, votes))
+            else:
+                for cid in self.congresses: 
+                    print("loading", cid, len(self.call_uid_map[cid].values()))
+                    votes = self.generate_dataset(self.call_uid_map[cid].values())
+                    graphs[issue].append((cid, votes))
+
 
         return graphs
 
@@ -37,25 +49,41 @@ class DatasetGenerator:
                 # os.makedirs(os.path.dirname(filename))
                 # numpy.save(filename, matrix)
 
-    def generate_dataset(self, issue, cid):
-        rollcalls = self.issue_map[issue][cid]
+    def generate_dataset(self, rollcalls):
         congressPeople = set()
 
         # Map from tuple of conggresspeople to number of times they have agreed.
         votes = collections.defaultdict(int)
 
         # Count all of the votes in the rollcall object.
+        all_senators = set()
         for rc in rollcalls:
             for key in rc.votes.keys():
-                self.countVotes(rc, key, votes)
+                for senator in rc.votes[key]: 
+                    all_senators.add(senator)
+                # self.countVotes(rc, key, votes)
+        sen_list = list(all_senators)
+        all_senators = {v:k for k, v in enumerate(sen_list)}
+        # print(all_senators)
+        # print(len(all_senators))
+        dim = len(all_senators)
+        matrix = np.zeros((dim, dim))
+        
+        for rc in rollcalls:
+            for key in rc.votes.keys():
+                vec = np.zeros(dim)
+                for senator in rc.votes[key]: 
+                    vec[all_senators[senator]] = 1
+                matrix += np.outer(vec, vec)
+        # print("mat", matrix)
+        return matrix, sen_list
 
-        return votes
-
-    def countVotes(self, rc, key, votes):
-        for i in range(len(rc.votes[key])):
-            for j in range(i+1, len(rc.votes[key])):
-                votepair = (rc.votes[key][i], rc.votes[key][j]) # Note that i < j.
-                votes[votepair]+= 1
+    # def countVotes(self, rc, key, votes):
+    #     for i in range(len(rc.votes[key])):
+    #         for j in range(i+1, len(rc.votes[key])):
+    #             votepair = (rc.votes[key][i], rc.votes[key][j]) # Note that i < j.
+    #             print(votepair)
+    #             votes[votepair]+= 1
 
     def generateAdjacencyMatrix(self, votes):
         nodes = set()
@@ -77,12 +105,14 @@ class DatasetGenerator:
             self.issue_map[issue] = collections.defaultdict(list)
             for uid in roll_call_uids: ## (congress#,rollcall#)
                 congressID = uid[0]
+                if congressID not in self.call_uid_map: continue
                 self.issue_map[issue][congressID].append(self.call_uid_map[congressID][uid])
 
     def load_congress_to_call_uid_map(self):
-        for i in range(1, 116):
-            # print(i)
+        for i in self.congresses:
+            print(i)
             self.call_uid_map[i] = self.load_pickle("./pickle/call_uid_map/congress_%d.pkl" % i)
+
 
 
 
--- lib/SpectralPartition.py
+++ lib/SpectralPartition.py
@@ -7,29 +7,83 @@ from DatasetGenerator import *
 import scipy.sparse
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+
+
+plt.style.use("ggplot")
 
 np.set_printoptions(suppress=True)
 class SpectralPartition:
     def __init__(self, projection):
         print("hello")
-        dg = DatasetGenerator(["Abortion"], range(90, 114))
+        issues = [
+            'Foreign and Defense Policy',
+            'Abortion',
+            'Pollution and Environmental Protection',
+            'Social Welfare',
+            'Medicaid',
+            'Civil Liberties',
+            'Israel',
+            'Firearms',
+            # 'Border security and unlawful immigration',
+            '',
+        ]
+        dg = DatasetGenerator(issues, range(65, 115))
         graphs = dg.generate_datasets()
         # print(graphs)
         # graphs: map<str issue, list<(congress, map<pair<senator>, int>)>>
-        for graph in graphs["Abortion"]:
-            congress, graph = graph
-            if sum(graph.values()) == 0: continue
-            adj_matrix, senators = self.get_adjacency_matrix(graph)
-            # adj_matrix = self.clean_adjacency_matrix(adj_matrix)
-            laplacian = scipy.sparse.csgraph.laplacian(adj_matrix, normed=True)
-            vals, vecs = np.linalg.eigh(laplacian)
-            colors = []
-            for i, senator in enumerate(senators):
-                # print(senator)
-                colors.append("br"[dg.member_party_map[(congress, senator)] == "Republican"])
-            x, y = vecs[:, 1:3].T
-            plt.scatter(x, y, c=colors)
-            plt.show()
+        congress_graphs = []
+        output_dir = "output_100y_test"
+        for issue in issues:
+            print(issue)
+
+            dirname = output_dir + "/vecs_%s" % issue.replace(" ", "_").lower()
+            if not os.path.isdir(dirname): os.makedirs(dirname)
+            congress_graph = []
+            for graph in graphs[issue]:
+                # print(graph)
+                congress, graph = graph
+                year = congress * 2 + 1788
+                if np.sum(graph[0]) == 0: continue
+                adj_matrix, senators = graph
+                if np.max(adj_matrix) < 10: continue
+                # adj_matrix = self.clean_adjacency_matrix(adj_matrix)
+                laplacian = scipy.sparse.csgraph.laplacian(adj_matrix, normed=True)
+                vals, vecs = np.linalg.eigh(laplacian)
+                colors = []
+                x, y = vecs[:, 1:3].T
+                dem_xs = []
+                for i, senator in enumerate(senators):
+                    # print(senator)
+                    party = dg.member_party_map[(congress, senator)]
+                    if party == "Democrat": 
+                        colors.append("b")
+                        dem_xs.append(x[i])
+                    elif party == "Republican": colors.append("r")
+                    else: colors.append("k")
+                if np.average(dem_xs) > 0: x = -x
+                if np.median(y) > 0: y = -y
+                plt.scatter(x, y, c=colors)
+                plt.savefig("%s/%d.png" % (dirname, year))
+                plt.cla(); plt.close()
+                congress_graph.append([year, vals[1]])
+            plt.plot(*np.array(congress_graph).T)
+            plt.ylim(0,1)
+            plt.savefig(output_dir + "/vals_%s.png" % issue.replace(" ", "_").lower())
+            plt.cla(); plt.close();
+            congress_graphs.append((issue, congress_graph))
+        colors = list(plt.cm.rainbow(np.linspace(0,1,len(issues)-1))) + ['k']
+        print(colors)
+        linewidth = 1
+        for i, ((issue, congress_graph), c) in enumerate(zip(congress_graphs, colors)):
+            if issue == '':
+                issue = 'ALL'
+                linewidth = 2
+            plt.plot(*np.array(congress_graph).T, label=issue, color=c,
+                        linewidth=linewidth)
+        plt.legend(fontsize="xx-small")
+        plt.ylim(0,1)
+        plt.savefig(output_dir + "/all_vals.png")
             
     def clean_adjacency_matrix(self, adj_matrix):
         adj_matrix = (adj_matrix > np.min(np.max(adj_matrix, axis=0), axis=0)/4).astype(float)
@@ -38,13 +92,13 @@ class SpectralPartition:
     def get_adjacency_matrix(self, graph):
         senators = set()
         for k in graph: senators |= set(k)
-        print(len(senators))
+        # print(len(senators))
         senators = list(sorted(senators))
         senator_to_index = {k:i for (i, k) in enumerate(senators)}
 
-        print(senators, senator_to_index)
+        # print(senators, senator_to_index)
         adj_matrix = np.zeros([len(senators), len(senators)])
-        print(adj_matrix)
+        # print(adj_matrix)
         for i, si in enumerate(senators):
             for j, sj in enumerate(senators[i+1:]):
                 item = graph[(si, sj)]
@@ -52,11 +106,11 @@ class SpectralPartition:
                 adj_matrix[i, i+j+1] = item
                 adj_matrix[i+j+1, i] = item 
         print(adj_matrix)
-        degrees = np.sum(adj_matrix, axis=0)
-        print(list(sorted(degrees)))
-        adj_matrix = adj_matrix[degrees > 5000,:]
-        adj_matrix = adj_matrix[:, degrees > 5000]
-        print(adj_matrix)
+        # degrees = np.sum(adj_matrix, axis=0)
+        # print(list(sorted(degrees)))
+        # adj_matrix = adj_matrix[degrees > 5000,:]
+        # adj_matrix = adj_matrix[:, degrees > 5000]
+        # print(adj_matrix)
         return adj_matrix, senators
 
 SpectralPartition(None)
